2854371d3fb04bf2bce5555d198babdc423ddc1c
moroccohabib <moroccohabib96@gmail.com>
2026-03-06 18:58:08 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nigeria-currency-recognition-system',
+  webDir: 'www'
+};
+
+export default config;
