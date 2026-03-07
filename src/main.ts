e444a1ecb4e0d09a5e3a9a802f0178c585e06645
moroccohabib <moroccohabib96@gmail.com>
2026-03-07 12:55:15 +0100

    added Buffer

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { Buffer } from 'buffer';
-
+(window as any).Buffer = Buffer;
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    
   ],
-  (window as any).Buffer = Buffer;
+  
 });
