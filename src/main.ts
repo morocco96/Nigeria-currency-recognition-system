adc85de868b586199b92bf10e6440ba1b3a427ce
moroccohabib <moroccohabib96@gmail.com>
2026-03-07 12:53:34 +0100

    Fix buffer dependency for Appflow build

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { Buffer } from 'buffer';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -11,4 +12,5 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
+  (window as any).Buffer = Buffer;
 });
